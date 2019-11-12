80799dfbc56b30d952ea19821430824ac4a312e4
unathi <unathi@byteorbit.com>
2019-11-12 17:42:29 +0200

    Adding the correct soccer_assignment file

---
+++ soccer_assignment.py
@@ -0,0 +1,61 @@
+import argparse
+import csv
+from collections import defaultdict
+from operator import itemgetter
+
+
+class SoccerLeague:
+    def split_data(self, input_file):
+        matches = []
+        for row in input_file:
+            match = [i.rsplit(' ', 1) for i in row]
+            matches.append(match)
+        return matches
+
+    def compare_scores(self, input_data):
+        unsorted_log = defaultdict(int)
+
+        for game in self.split_data(input_data):
+            opponent1_name = game[0][0]
+            opponent2_name = game[1][0]
+            opponent1_score = game[0][1]
+            opponent2_score = game[1][1]
+
+            if opponent1_score > opponent2_score:
+                unsorted_log[opponent1_name] += 3
+                unsorted_log[opponent2_name] += 0
+
+            elif opponent1_score == opponent2_score:
+                unsorted_log[opponent1_name] += 1
+                unsorted_log[opponent2_name] += 1
+
+        return unsorted_log
+
+    def sort_results(self, input_data):
+        log = self.compare_scores(input_data)
+        sorted_data = sorted(log.items(), key=itemgetter(0))
+        rankings = sorted(sorted_data, key=itemgetter(1), reverse=True)
+        return rankings
+
+    def rank_teams(self, input_data):
+        final_standings = ''
+        rankings = self.sort_results(input_data)
+        for rank in rankings:
+            if rank[1] == 1:
+                points_suffix = "pt"
+            else:
+                points_suffix = "pts"
+
+            final_standings += '{}. {}, {} {}\n'.format(rankings.index(rank) + 1, rank[0], rank[1], points_suffix)
+
+        print(final_standings)
+        return final_standings
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    match_results = csv.reader(args.file)
+    sl = SoccerLeague()
+    sl.rank_teams(match_results)
